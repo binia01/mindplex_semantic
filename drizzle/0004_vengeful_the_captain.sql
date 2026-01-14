3f9a169a2de5f97d28846bb6c717bf4486aa35f2
Dawit Mekonnen <56738450+Dave-lab12@users.noreply.github.com>
2026-01-14 12:55:52 +0300

    fix failing migration

--- drizzle/0004_vengeful_the_captain.sql
+++ drizzle/0004_vengeful_the_captain.sql
@@ -1,5 +1,5 @@
 ALTER TABLE "article_chunks" RENAME COLUMN "embedded_content" TO "chunk_to_embed";--> statement-breakpoint
-ALTER TABLE "article_chunks" ADD COLUMN "search_vector" "tsvector" GENERATED ALWAYS AS (to_tsvector('english', chunkToEmbed)) STORED;--> statement-breakpoint
+ALTER TABLE "article_chunks" ADD COLUMN "search_vector" "tsvector" GENERATED ALWAYS AS (to_tsvector('english', chunk_to_embed)) STORED;--> statement-breakpoint
 ALTER TABLE "articles" ADD COLUMN "search_vector" "tsvector" GENERATED ALWAYS AS (setweight(to_tsvector('english', coalesce(title, '')), 'A') || 
             setweight(to_tsvector('english', coalesce(teaser, '')), 'B')) STORED;--> statement-breakpoint
 CREATE INDEX "chunks_search_idx" ON "article_chunks" USING gin ("search_vector");--> statement-breakpoint
